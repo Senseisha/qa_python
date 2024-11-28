a7f4659072b62e75d0f9948bf7eaf60950271ff8
Anastasia Troitskaya <senseisha@yandex.ru>
2024-11-28 15:01:44 +0300

    Добавлен тест на вывод списка Ñкниг с определенным жанром

--- tests.py
+++ tests.py
@@ -24,22 +24,19 @@ class TestBooksCollector:
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
 
 
-    def test_add_new_book_len_more_then_40_false(self):
-        collector = BooksCollector()
+    def test_add_new_book_len_more_then_40_false(self, collector):
         name = 'Что делать, если ваш кот хочет вас убить и съесть'
         collector.add_new_book(name)
 
         assert collector.books_genre.get(name) is None
 
-    def test_add_new_book_without_genre_true(self):
-        collector = BooksCollector()
+    def test_add_new_book_without_genre_true(self, collector):
         name = 'Идиот'
         collector.add_new_book(name)
 
         assert collector.books_genre[name] == ''
 
-    def test_set_book_genre_if_book_in_books_genre_and_her_genre_in_genre_true(self):
-        collector = BooksCollector()
+    def test_set_book_genre_if_book_in_books_genre_and_her_genre_in_genre_true(self, collector):
         name = '12 стульев'
         genre = 'Комедии'
         collector.add_new_book(name)
@@ -47,8 +44,7 @@ class TestBooksCollector:
 
         assert collector.books_genre.get(name) == genre
 
-    def test_test_set_book_genre_if_book_in_books_genre_and_her_genre_not_in_genre_true(self):
-        collector = BooksCollector()
+    def test_test_set_book_genre_if_book_in_books_genre_and_her_genre_not_in_genre_true(self, collector):
         name = '12 стульев'
         genre = 'Мелодрама'
         collector.add_new_book(name)
@@ -56,10 +52,26 @@ class TestBooksCollector:
 
         assert collector.books_genre.get(name) != genre
 
-    def test_get_book_genre_true(self):
-        collector = BooksCollector()
+    def test_get_book_genre_true(self, collector):
         name = '12 стульев'
         collector.add_new_book(name)
         book_genre = collector.get_book_genre(name)
 
         assert book_genre == collector.books_genre.get(name)
+
+    def test_get_books_with_specific_genre(self, collector):
+        amount_of_fantastic = 2
+
+        collector.add_new_book('12 стульев')
+        collector.set_book_genre('12 стульев', 'Комедии')
+
+        collector.add_new_book('Дюна')
+        collector.set_book_genre('Дюна', 'Фантастика')
+
+        collector.add_new_book('Гарри Поттер')
+        collector.set_book_genre('Гарри Поттер', 'Фантастика')
+
+        specific_genre = collector.get_books_with_specific_genre('Фантастика')
+
+        assert len(specific_genre) == amount_of_fantastic
+
